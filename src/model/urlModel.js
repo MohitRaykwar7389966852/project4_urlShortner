1f81fe1a1a4f280ee7ed9378df953ed5da3408b1
MohitRaykwar7389966852 <mohitraykwar99@gmail.com>
2022-05-17 16:28:27 +0530

    schema

---
+++ src/model/urlModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const urlSchema = new mongoose.Schema({
+    "urlCode":{type:String,required:true,unique:true,lowercase:true,trim:true},
+    "longUrl":{type:String,required:true},
+    "shortUrl":{typr:String , required:true , unique:true}
+},{timestamps:true})
+
+module.exports = mongoose.model("UrlShort", urlSchema);
