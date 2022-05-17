9329893ae114e307dae661f3819e49274b5e1fec
SariqueZamal <sariquezamal0@gmail.com>
2022-05-17 16:48:39 +0530

    yes

---
+++ src/controller/urlController.js
@@ -0,0 +1,9 @@
+const validUrl = require('valid-url')
+const shortid = require('shortid')
+
+
+
+
+
+
+// module.exports = {  };
---
+++ src/route/route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// const { } = require("../controller/urlController")
+
+
+router.post("/url/shorten",)
+// router.get("/functionup/colleges", collegeController.createCollege)
+// router.get("/functionup/collegeDetails",collegeController.collegeDetails)
+
+
+module.exports = router;
