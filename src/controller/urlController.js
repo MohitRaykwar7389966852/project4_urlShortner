e5d92f30ad97b68b70afed567bab704393520308
SariqueZamal <sariquezamal0@gmail.com>
2022-05-19 11:01:40 +0530

    update

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -21,8 +21,6 @@ redisClient.on("connect", async function () {
     console.log("Connected to Redis..");
 });
 
-//Connection setup for redis
-
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
@@ -35,26 +33,39 @@ const urlShortner = async function (req, res) {
         if (Object.keys(requestBody).length === 0) {
             return res.status(400).send({ status: false, message: "No data provided" });
         }
-
         if (Object.keys(requestBody).length > 1) {
-            return res.status(400).send({ status: false, message: "Please Enter the Long Url Only" });
+            return res.status(400).send({ status: false, message: "Please Enter the longUrl Only" });
         }
 
         const longUrl = requestBody.longUrl;
-        const baseUrl = "http://localhost:3000"
+        const baseUrl = "http://localhost:3000";
+        const urlCode = shortid.generate().toLowerCase();
+        const shortUrl = baseUrl + '/' + urlCode;
 
+        if (!longUrl) {
+            return res.status(400).send({ status: false, message: 'Please Enter the longUrl' })
+        }
         if (!validUrl.isUri(longUrl)) {
             return res.status(400).send({ status: false, message: 'Invalid longUrl' })
         }
 
-        //Before creating the short URL,we check if the long URL was in the DB ,else we create it.
-        const urlAlreadyInDb = await urlModel.findOne({ longUrl }).select({ shortUrl: 1, longUrl: 1, urlCode: 1, _id: 0 });
+         // Before creating the short URL,we check if the long URL was in the Cache ,else we will check in the DB.
 
+        let urlAlreadyInCache = await GET_ASYNC(`${longUrl}`)
+        if (urlAlreadyInCache) {
+            return res.status(200).send({status: true, message: 'Url Already In Cache',data: JSON.parse(urlAlreadyInCache)})
+        }
+
+        // Before creating the short URL,we check if the long URL was in the DB ,else we create it.
+
+        const urlAlreadyInDb = await urlModel.findOne({ longUrl }).select({ shortUrl: 1, longUrl: 1, urlCode: 1, _id: 0 });
         if (urlAlreadyInDb) {
-            return res.status(200).send({ status: true, message: 'Url Shorten Successfully', data: urlAlreadyInDb })
+
+            await SET_ASYNC(`${longUrl}`, JSON.stringify(urlAlreadyInDb), "EX",90);
+            await SET_ASYNC(`${urlCode}`, JSON.stringify(urlAlreadyInDb.longUrl), "EX",90);
+
+            return res.status(200).send({ status: true, message: 'Url Already In Db', data: urlAlreadyInDb })
         } else {
-            const urlCode = shortid.generate().toLowerCase();
-            const shortUrl = baseUrl + '/' + urlCode;
 
             const newUrl = {
                 longUrl: longUrl.trim(),
@@ -63,6 +74,10 @@ const urlShortner = async function (req, res) {
             }
 
             const urlCreated = await urlModel.create(newUrl);
+
+            await SET_ASYNC(`${longUrl}`, JSON.stringify(newUrl), "EX",90);
+            await SET_ASYNC(`${urlCode}`, JSON.stringify(newUrl.longUrl), "EX",90);
+
             return res.status(201).send({ status: true, message: 'Url Shorten Successfully', data: urlCreated })
         }
     } catch (error) {
@@ -76,9 +91,9 @@ const urlRedirect = async function (req, res) {
     try {
         let urlCode = req.params.urlCode
 
-        let cahcedUrlCode = await GET_ASYNC(urlCode)
-        if (cahcedUrlCode) {
-            return res.status(302).redirect(cahcedUrlCode)
+        let urlCodeAlreadyInCache = await GET_ASYNC(`${urlCode}`)
+        if (urlCodeAlreadyInCache) {
+            return res.status(302).redirect(JSON.parse(urlCodeAlreadyInCache))
         } else {
 
             let findUrl = await urlModel.findOne({ urlCode: urlCode })
@@ -86,7 +101,7 @@ const urlRedirect = async function (req, res) {
                 return res.status(404).send({ status: false, message: "Url Code not found" })
             }
 
-            await SET_ASYNC(urlCode, findUrl.longUrl)
+            await SET_ASYNC(`${urlCode}`, JSON.stringify(findUrl.longUrl), "EX",90);
             return res.status(302).redirect(findUrl.longUrl)
         }
     } catch (error) {
